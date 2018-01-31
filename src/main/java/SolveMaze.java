22ee72e236b527a5bbfa0a8bda0ace6626573742
edt2 <edt2@illinois.edu>
2018-01-31 16:39:40 -0600

    Made a while loop code so that the direction keeps turning left until it can move forward. Once canMove became true it would move forward. just made an extra comment.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -16,6 +16,7 @@ public class SolveMaze {
      * @param unused unused input arguments
      */
 
+
     public static void main(final String[] unused) {
         /*
          * Create a new 10 x 10 maze. Feel free to change these values.
@@ -32,6 +33,9 @@ public class SolveMaze {
         /*
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
+         */
+        /* simplified my old code
+
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
